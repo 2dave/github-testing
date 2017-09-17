5a0a08d636884b7ef6a5be17a08fc63a5156973f
David Eidson <dave.eidson@gmail.com>
2017-09-17 18:37:53 -0500

    What's next? :)

--- lister/Program.cs
+++ lister/Program.cs
@@ -34,6 +34,21 @@ namespace lister
                     {
                         Console.WriteLine(Encoding.UTF8.GetString(b));
                     } 
+
+            // Well it took a bit but now I can seem to work in the environment now. I would
+            // like a review somtime on how you work on multiple machines sometimes (with the same code)
+            // and what that workflow looks like. 
+            // Also some questions on working with Visual Studio Code. On a desktop I have
+            // I cannot use debugging and it took me a long time to figure out how to go to 
+            // TERMINAL and run 'dotnet run' (after building) to get the code to execute. 
+            //
+            // I created my own branch to work on and pushed it to Github. I'm not sure what the
+            // workflow looks like to merge it back with master or when I should. Should I just 
+            // keep cloning that branch and pushing it back as I work?
+            //
+            // Let me know what's next. Finally feels good getting past some of the administrative 
+            // stuff and now I can actually code something! Getting VS Code to work on another 
+            // machine was a bear - but I am getting a handle on it now. 
             }
          }
     }
