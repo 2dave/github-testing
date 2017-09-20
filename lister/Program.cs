2a882d4ad59ba8f3caa2443e3818857e04a7c9c0
Eidson <eidsond@umsystem.edu>
2017-09-19 20:15:25 -0500

    Included a try catch block. One catch block for the 'file not found' and one for general exceptions. It was a little tough looking at best practices for error handling in c# as there were many different opinions. Let me know if you have a better way.
    Fixes #8

--- lister/Program.cs
+++ lister/Program.cs
@@ -13,21 +13,25 @@ namespace lister
 
             Console.WriteLine("Import data from the simple-3-list.txt file in the data directory");
 
-            if (!File.Exists(path))
+            try
             {
-                Console.WriteLine("simple-3-list.txt does not exist in \\data directory");
-            }
-  
-            using (FileStream myFile = File.OpenRead(path))
-            {
-                byte[] b = new byte[1024];
-              
-                while (myFile.Read(b,0,b.Length) > 0)
+                using (FileStream myFile = File.OpenRead(path))
+                {
+                    byte[] b = new byte[1024];
+                    while (myFile.Read(b,0,b.Length) > 0)
                     {
                         Console.WriteLine(Encoding.UTF8.GetString(b));
-                    } 
-
+                    }
+                }
             }
-         }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("FILE NOT FOUND IN .\\data");
+            }
+            catch (Exception ex)
+            {
+                 Console.WriteLine(ex);
+            }  
+        }
     }
 }
