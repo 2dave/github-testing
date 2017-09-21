246b63f012f604675a88e38c9fc1177a665a0e6d
Eidson <eidsond@umsystem.edu>
2017-09-20 22:21:13 -0500

    Adding another way to take the contents of a file and displaying it on screen.
    I have added a streamreader object and used it to populate a new string array. Then I display the new array in the console window.

--- lister/Program.cs
+++ lister/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Collections.Generic;
 
 namespace lister
 {
@@ -8,16 +9,16 @@ namespace lister
     {
         static void Main(string[] args)
         {
-            // This file is included as Content in the project file so it is copied to the output path
-            // as part of the build.
             var path = @".\data\simple-3-list.txt";
 
-            Console.WriteLine("Import data from the simple-3-list.txt file in the data directory");
+            Console.WriteLine("Importing data from the simple-3-list.txt file in the data directory.");
 
+            // Using two different ways to get the output of a file on the console screen. 
             try
             {
                 using (FileStream myFile = File.OpenRead(path))
                 {
+                    Console.WriteLine("Using Read on a file stream to populate a byte array. Then displaying the byte array.");
                     byte[] b = new byte[1024];
                     int read;
 
@@ -26,7 +27,32 @@ namespace lister
                         Console.WriteLine(Encoding.UTF8.GetString(b, 0, read));
                     }
                 }
+
+                using (StreamReader file = new StreamReader(path))
+                {
+                    Console.WriteLine("Using StreamReader to populate a new string array and then displaying that array.");
+                    string line;
+
+                    // Taken from a stackoverflow example. I have not used this generic collection before but 'List' 
+                    // seemed simple enough to use and manipulate.
+                    var list = new List<string>();
+
+                    while((line = file.ReadLine()) != null)
+                        {
+                            //Console.WriteLine (line);
+                            list.Add(line);
+                        } 
+                    string[] result = list.ToArray();
+
+                    // Using 'foreach' is new to me. I could not translate it into an equivalent 'for' loop. I would like to know how
+                    // to convert it into a regular 'for' loop so that I understand the syntax of 'foreach' better.
+                    foreach (var item in result)
+                    {
+                        Console.WriteLine(item.ToString());
+                    }
+                }
             }
+            
             catch (FileNotFoundException e)
             {
                 Console.WriteLine("Could not find: {0}", e.FileName);
