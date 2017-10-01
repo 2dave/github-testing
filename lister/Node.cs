e431c3669b2c7873699cb1a9043068c4e67b1f6c
David Eidson <dave.eidson@gmail.com>
2017-10-01 17:36:20 -0500

    Adding an internal Node class and a public LinkedList class
    I am going to implement a doubly linked list data structure in this
    project.

--- lister/Node.cs
+++ lister/Node.cs
@@ -1,10 +1,23 @@
 namespace TwoDave.Lister
 {
     // I don't think I can use 'internal' for this class because if I do then I cannot instantiate it from any other .cs file right?
-    public class Node
+    // On second thought, I think because it is in the same namespace it will. 
+    internal class Node
     {
         public Node next;
-        public object data;
-        
+        public object data;     
+    }
+
+    public class LinkedList
+    {
+        private Node head;
+        private Node current;
+        public int count;
+
+        public LinkedList()
+        {
+            
+        }
+
     }
 }
