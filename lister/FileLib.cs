9c6e64e02659a1cb028b880113602756c3467434
David Eidson <dave.eidson@gmail.com>
2017-10-01 17:13:03 -0500

    Removed #if ... #endif block
    The block was for my own knowledge and I wanted to be able to write that
    note down later when I orignally made it. Now I got it copied so I went
    back to clean it up.

--- lister/FileLib.cs
+++ lister/FileLib.cs
@@ -3,23 +3,8 @@ using System.Collections.Generic;
 
 namespace TwoDave.Lister
 {
-    internal class FileLib
+    internal class FileLib 
     {
-
-#if add_to_onenote
-        // Add to OneNote
-        private FileLib()
-        {
-            // default constructor
-        }
-
-        public static FileLib Create()
-        {
-            var t = new FileLib();
-            return t;
-        }
-#endif
-
         public List<string> ReadFile(string path)
         {
             var list = new List<string>();
