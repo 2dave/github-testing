4355c232319cbeeaa6e1c48a2c7d4149007a5e9d
David Eidson <dave.eidson@gmail.com>
2017-10-01 17:25:11 -0500

    Changed Node class from internal to public
    If I leave the Node class as internal then (from what I read) it will
    not be able to be instantiated from any other .cs file.

--- lister/Node.cs
+++ lister/Node.cs
@@ -1,8 +1,10 @@
 namespace TwoDave.Lister
 {
-    internal class Node
+    // I don't think I can use 'internal' for this class because if I do then I cannot instantiate it from any other .cs file right?
+    public class Node
     {
         public Node next;
+        public object data;
         
     }
 }
