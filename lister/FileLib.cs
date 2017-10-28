f63a3f63439c391d69c9e51c2f2eb2482b70d5fd
Eidson <eidsond@umsystem.edu>
2017-10-27 21:23:54 -0500

    Disregarded the NodeCollection class and just made a List object of Nodes
    Trying something different as I couldn't get my head around a NodeCollection class. However, a list of Nodes was easy enough to work with.

--- lister/FileLib.cs
+++ lister/FileLib.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Collections.Generic;
 
@@ -7,7 +8,7 @@ namespace TwoDave.Lister
     {
         public NodeCollection ReadFile(string path)
         {
-            NodeCollection templist = new NodeCollection();            
+            NodeCollection templist = new NodeCollection(); // currently not using    
 
             using (StreamReader file = new StreamReader(path))
             {
@@ -16,13 +17,21 @@ namespace TwoDave.Lister
                 while ((line = file.ReadLine()) != null)
                 {
                     var temp = line.Split(' ');
-                    List<Node> newnodes = new List<Node>(); //unusual syntax
-                    Node tempnode = new Node();
-                    tempnode.data = temp[0];
-                    newnodes.Add(tempnode);
-                    //templist.Add(temp[0]);
-                    //templist.Add(temp[1]);
-                    //templist.Add(temp[2]);
+
+                    List<Node> newnodes = new List<Node>(); 
+                    Node tempnode1 = new Node();
+                    Node tempnode2 = new Node();
+
+                    tempnode1.data = temp[0];
+                    tempnode2.data = temp[2];
+
+                    newnodes.Add(tempnode1);
+                    newnodes.Add(tempnode2);
+
+                    foreach (Node n in newnodes)
+                    {
+                        Console.WriteLine("The list of nodes --> {0}", n.data.ToString());
+                    }
                 }
             }
             return templist;
