39ed261989ab4913e5e94f76a684fed02b01cb42
Eidson <eidsond@umsystem.edu>
2018-01-07 21:16:34 -0600

    Refactored CanFindCorrectHeadNode Unit test
    Created GenerateNodes helper function for this unit test and refactored the unit test.

--- lister.test/FileLibFixture.cs
+++ lister.test/FileLibFixture.cs
@@ -7,7 +7,7 @@ namespace TwoDave.Lister.Test
 {
     public class FileLibFixture
     {
-       
+
         [Fact]
         public void CanHandleBadPath()
         {
@@ -42,17 +42,17 @@ namespace TwoDave.Lister.Test
             Node HeadNodeTest = FileLib.ReadCommand(path);
 
             //REVERSE ORDER - can't start with { data = "a", next = b } because b won't exist at that time            
-            Node h = new Node {data = "h", next = null};
-            Node g = new Node {data = "g", next = h};
-            Node f = new Node {data = "f", next = g};
-            Node e = new Node {data = "e", next = f};
-            Node d = new Node {data = "d", next = e};
-            Node c = new Node {data = "c", next = d};
-            Node b = new Node {data = "b", next = c};
-            Node a = new Node {data = "a", next = b};
+            //Node h = new Node { data = "h", next = null };
+            //Node g = new Node { data = "g", next = h };
+            //Node f = new Node { data = "f", next = g };
+            //Node e = new Node { data = "e", next = f };
+            //Node d = new Node { data = "d", next = e };
+            //Node c = new Node { data = "c", next = d };
+            //Node b = new Node { data = "b", next = c };
+            //Node a = new Node { data = "a", next = b };
 
-            //var data2 = new[] { "a", "b", "c", "d", "e", "f", "g", "h" };
-            //Node a = GenerateNodes(data2);
+            var data2 = new[] { "a", "b", "c", "d", "e", "f", "g", "h" };
+            Node a = GenerateNodes(data2);
 
             string MyAnswer = FileLib.GenerateString(a);
             string TestAnswer = FileLib.GenerateString(HeadNodeTest);
@@ -73,39 +73,42 @@ namespace TwoDave.Lister.Test
         [Fact]
         public void CanGenerateNodes()
         {
-            string[] data = new string[3];
-            data[0] = "a";
-            data[1] = "b";
-            data[2] = "c";
+            //string[] data = new string[3];
+            //data[0] = "a";
 
             //string[] data3 = {"a", "b", "c"};
-            var data2 = new[] { "a", "b", "c" };
+            var data2 = new[] { "a", "b", "c", "d", "e", "f", "g", "h" };
 
             Node generated = GenerateNodes(data2);
-
         }
 
-
-        //TODO
         private Node GenerateNodes(string[] input)
         {
             Node head = null;
-            Node past = null;
+            Node previous = null;
 
             foreach (string n in input)
             {
                 Node current = new Node();
-                current.data = n;
+
+                current.data = n; // current -> a.data = first string
+
+                if (previous == null)
+                {
+                    previous = current;
+                }
 
                 if (head == null)
                 {
-                    head = current;
+                    head = current; // a is now head
                 }
 
+                previous.next = current;
+
+                previous = current;
             }
 
             return head;
         }
-
     }
 }
